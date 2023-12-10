c250dfd57067e25e7eed49c9a0c9897694937f51
TomasUesato <uesatotomas@gmail.com>
2023-12-10 00:21:22 -0300

    Day 2, part 2 solution

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -1,6 +1,6 @@
-MAX_RED = 12
-MAX_GREEN = 13
-MAX_BLUE = 14
+RED_CUBES = 12
+GREEN_CUBES = 13
+BLUE_CUBES = 14
 
 
 def is_game_set_valid(game_set):
@@ -13,19 +13,22 @@ def is_game_set_valid(game_set):
 def is_subset_valid(subset):
     cubes = subset.split(",")
     for cube in cubes:
-        number, color = cube.split()
-        number = int(number)
-
-        if color == "green" and MAX_GREEN < number:
+        number, color = get_cube_info(cube)
+        if color == "green" and GREEN_CUBES < number:
             return False
-        if color == "red" and MAX_RED < number:
+        if color == "red" and RED_CUBES < number:
             return False
-        if color == "blue" and MAX_BLUE < number:
+        if color == "blue" and BLUE_CUBES < number:
             return False
     return True
 
 
-def main():
+def get_cube_info(cube):
+    number, color = cube.split()
+    return int(number), color
+
+
+def part1():
     total_sum = 0
     with open('input.txt', 'r') as file:
         for line in file:
@@ -41,5 +44,35 @@ def main():
     return total_sum
 
 
+def part2():
+    power_sum = 0
+    with open('input.txt', 'r') as file:
+        for line in file:
+            sets = line.split(":")[1]
+            game_set = sets.split(";")
+            power_sum += get_power(game_set)
+
+    return power_sum
+
+
+def get_power(game_set):
+    max_red = 0
+    max_blue = 0
+    max_green = 0
+
+    for subset in game_set:
+        cubes = subset.split(",")
+        for cube in cubes:
+            number, color = get_cube_info(cube)
+            if color == "red" and number > max_red:
+                max_red = number
+            if color == "green" and number > max_green:
+                max_green = number
+            if color == "blue" and number > max_blue:
+                max_blue = number
+
+    return max_red * max_green * max_blue
+
+
 if __name__ == "__main__":
-    print(main())
+    print(part2())
