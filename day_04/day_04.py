10ebd024106e997562fc1fa08ae04c9c18f45119
TomasUesato <uesatotomas@gmail.com>
2023-12-16 17:22:48 -0300

    Day 04, part 2 solution

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -14,5 +14,30 @@ def part1():
     return total_points
 
 
+def part2():
+    with open("input.txt") as file:
+        content = file.read()
+        lines = content.split("\n")
+        num_cards = len(lines)
+        cards = [1] * num_cards
+
+        for card_index, card_data in enumerate(lines):
+            winning_numbers_str, numbers_str = card_data.split("|")
+            numbers = set(numbers_str.split())
+
+            winning_numbers_set = set(
+                winning_numbers_str.split(":")[1].split())
+
+            matching_numbers = sum(
+                1 for number in numbers if number in winning_numbers_set)
+
+            for i in range(1, matching_numbers + 1):
+                next_card_index = card_index - 1 + i
+                if next_card_index < num_cards:
+                    cards[next_card_index] += cards[card_index - 1]
+
+    return sum(cards)
+
+
 if __name__ == "__main__":
-    print(part1())
+    print(part2())
